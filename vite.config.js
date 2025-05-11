4ad6d10231e1ca772fd522ca62cffc39d9a6415a
Prabath Rathnamalala <prabathrathnamalala2k2@gmail.com>
2025-05-11 08:31:09 +0530

    updted

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  css: {
-    devSourcemap: true,
-  },
+  base: './', // Ensures assets are loaded correctly
   build: {
-    sourcemap: true,
-    cssCodeSplit: true,
+    outDir: 'dist', // Output directory for the build
   },
   server: {
-    open: true,
-    host: true
-  }
+    port: 3000,
+  },
 });
